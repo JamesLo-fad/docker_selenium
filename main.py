d5953b62f48ccdcb77714a55fad379882b9a4cc6
James Lo <llotszfor12346@gmail.com>
2022-02-19 13:36:23 +0800

    5th commit

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+chrome_options = Options()
+driver = webdriver.Chrome(service=s, options=chrome_options)
+
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+
 
 driver.get("https://www.hktvmall.com/")
 
@@ -13,5 +20,7 @@ elem = driver.find_element(By.CSS_SELECTOR, '.SuggestionSearch-input')
 
 elem.send_keys("魚")
 elem.send_keys(Keys.ENTER)
+
+driver.quit()
 
 print("done")
